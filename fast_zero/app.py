18a5254ef9855ce489494deed243ca4def1afade
Carvalho <guilhermeclcosta@hotmail.com>
2026-08-18 00:55:31 -0300

    Produzindo o desafio da aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,19 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá, mundo!"}
+
+
+@app.get("/hello")
+def desafio_html():
+    return """
+        <h1>Olá, mundo!</h1>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,11 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.json() == {"message": "Olá, mundo!"}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_desafio_deve_retornar_ola_mundo():
+    new_client = TestClient(app)
+    
+    response = new_client.get("/hello")
+    
+    assert response.json() == "<h1>Olá, mundo!</h1>"
