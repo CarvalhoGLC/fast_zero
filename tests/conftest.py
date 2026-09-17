aa6c75c6740cd16f068ce35783cd30f247c541ea
Carvalho <guilhermeclcosta@hotmail.com>
2026-09-17 12:48:28 -0300

    Resolvendo exercício da aula 4

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,8 +31,9 @@ def session():
 def _mock_db_time(*, model, time=datetime(2026, 9, 16)):
 
     def fake_time_hook(mapper, connection, target):
-        if hasattr(target, "created_at"):
+        if hasattr(target, "created_at") and hasattr(target, "updated_at"):
             target.created_at = time
+            target.updated_at = time
 
     event.listen(model, "before_insert", fake_time_hook)
 
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+from datetime import datetime
 
 from sqlalchemy import select
 
@@ -9,7 +10,10 @@ def test_create_user(session, mock_db_time):
 
     with mock_db_time(model=User) as time:
         new_user = User(
-            username="Guilherme", email="gui@example.com", password="123456"
+            username="Guilherme",
+            email="gui@example.com",
+            password="123456",
+            updated_at=datetime.now(),
         )
         session.add(new_user)
         session.commit()
@@ -22,4 +26,5 @@ def test_create_user(session, mock_db_time):
         "email": "gui@example.com",
         "password": "123456",
         "created_at": time,
+        "updated_at": time,
     }
